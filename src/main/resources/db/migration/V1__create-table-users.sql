741ecce97937ae641cd041e268b5c4b431d71ac5
Durian Sosa <dmsosa21@outlook.com>
2023-11-15 19:51:47 +0000

    Creating Migrations

---
+++ src/main/resources/db/migration/V1__create-table-users.sql
@@ -0,0 +1,10 @@
+CREATE TYPE role as ENUM ('admin', 'basic');
+
+CREATE TABLE IF NOT EXISTS users (
+id TEXT UNIQUE PRIMARY KEY
+username VARCHAR(40) UNIQUE NOT NULL,
+email VARCHAR(100) UNIQUE NOT NULL,
+password VARCHAR(40) NOT NULL,
+bio VARCHAR(1500),
+user_role role
+);
