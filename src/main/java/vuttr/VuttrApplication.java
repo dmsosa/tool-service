f7e2fb7427ac881605a63b0e1057a2d9b90a4814
Durian Sosa <dmsosa21@outlook.com>
2023-11-26 20:28:41 +0000

    Config imported from Config Server

--- src/main/java/vuttr/VuttrApplication.java
+++ src/main/java/vuttr/VuttrApplication.java
@@ -3,9 +3,11 @@ package vuttr;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class VuttrApplication {
 
 	public static void main(String[] args) {
